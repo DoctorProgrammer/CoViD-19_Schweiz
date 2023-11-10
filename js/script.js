255a5f8104b5f794943be9716a370f7ba494d340
Robin <49040626+DoctorProgrammer@users.noreply.github.com>
2023-11-10 10:28:22 +0100

    Initial commit
    First commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Author: Robin Trachsel
+ * version: 1.0
+ * date: 2023-11-10
+ * 
+ * Description: Script for the style of the website
+ */
+
+document.addEventListener('DOMContentLoaded', function() {
+    // On load
+});
