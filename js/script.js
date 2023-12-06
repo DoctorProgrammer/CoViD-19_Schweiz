192bf890666708bb475ce2033f8ce95ffa69df32
trachselr <trachselr@bzz.ch>
2023-12-06 11:06:18 +0100

    UPDATED: Design on index.html

--- js/script.js
+++ js/script.js
@@ -7,5 +7,31 @@
  */
 
 document.addEventListener('DOMContentLoaded', function() {
-    // On load
+    const logo = document.getElementById('logo');
+
+    const navButtons = document.querySelectorAll('.dropbtn');
+    const dropdownContent = document.querySelectorAll('.dropdown-content');
+
+    logo.addEventListener('click', function() {
+        // go to startpage
+        window.location.href = '/';
+    });
+
+    navButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            // copy the dropdown-content sibling class, and paste it into the dropdown-content id
+            let content = button.nextElementSibling;
+            let id = document.getElementById('dropdown-content');
+
+            if(id.innerHTML != content.innerHTML) {
+                id.innerHTML = content.innerHTML;
+                id.style.height = '200px';
+                id.style.margin = '1rem 1rem';
+            } else {
+                id.innerHTML = '';
+                id.style.height = '0px';
+                id.style.margin = '0 1rem';
+            }
+        });
+    });
 });
